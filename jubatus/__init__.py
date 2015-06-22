7d8597e3fa1038db607e8b56e4e6c333ee272ad1
Hiroki KIYOHARA <hirokiky@gmail.com>
2015-06-22 14:12:32 +0900

    Added idle files

---
+++ jubatus/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ["anomaly", "bandit", "burst", "classifier", "clustering",
+           "graph", "nearest_neighbor", "recommender", "regression", "stat", ]
+
+from jubatus.anomaly.client import Anomaly  # NOQA
+from jubatus.bandit.client import Bandit  # NOQA
+from jubatus.burst.client import Burst  # NOQA
+from jubatus.classifier.client import Classifier  # NOQA
+from jubatus.clustering.client import Clustering  # NOQA
+from jubatus.graph.client import Graph  # NOQA
+from jubatus.nearest_neighbor.client import NearestNeighbor  # NOQA
+from jubatus.recommender.client import Recommender  # NOQA
+from jubatus.regression.client import Regression  # NOQA
+from jubatus.stat.client import Stat  # NOQA
